07cf4a61753d2a0518b7b84e016dfb10ad0ca26e
cbeAbishek <agsabhishek1907@gmail.com>
2025-06-25 11:42:41 +0530

    local

--- app/request-quote/page.tsx
+++ app/request-quote/page.tsx
@@ -1,6 +1,10 @@
 import RequestQuotePage from "@/components/requestform/Quote";
 import ProductsPage from "@/components//requestform/rq";
 
+export const metadata = {
+  title: 'Request a Quote | Cloves Inc.',
+  description: 'Get a personalized quote from Cloves Inc. for autoclave liners, industrial packaging, or commercial textiles. Fill out our form or contact us directly for fast assistance.',
+};
 
 export default function Request() {
   return (
--- app/whyus/page.tsx
+++ app/whyus/page.tsx
@@ -1,6 +1,9 @@
 import WhyChooseUs  from '@/components/whyus/main';
 
-
+export const metadata = {
+  title: 'Why Choose Cloves Inc. | Trusted Packaging & Textile Solutions',
+  description: 'Discover why industries around the world trust Cloves Inc. for innovative, high-quality, and sustainable packaging and textile products tailored to their unique needs.',
+};
 
 export default function Home() {
   return (
