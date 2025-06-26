01615c62f9031867e30bcd05d27b6bb24aa2c50f
sarran528 <sarranm528@gmail.com>
2025-06-27 02:13:45 +0530

    main swaped folder

---
+++ app/products/page.tsx
@@ -0,0 +1,15 @@
+import ProductsPage from '@/components/Products/commonproduct';
+
+export const metadata = {
+  title: 'Cloves Inc. Products | Industrial Packaging & Textiles',
+  description: 'Explore Cloves Inc.’s full range of products including autoclave liners, bulk bags, jute/hessian fabrics, poly-woven materials, and commercial textiles for healthcare and industrial use.',
+};
+
+export default function Home() {
+  return (
+    <>
+
+      <ProductsPage />
+    </>
+  );
+}
---
+++ app/whyus/page.tsx
@@ -0,0 +1,14 @@
+import WhyChooseUs  from '@/components/whyus/main';
+
+export const metadata = {
+  title: 'Why Choose Cloves Inc. | Trusted Packaging & Textile Solutions',
+  description: 'Discover why industries around the world trust Cloves Inc. for innovative, high-quality, and sustainable packaging and textile products tailored to their unique needs.',
+};
+
+export default function Home() {
+  return (
+    <>
+        <WhyChooseUs  />
+    </>
+  );
+}
